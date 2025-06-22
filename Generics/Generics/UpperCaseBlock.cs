5830bfc6bb6b95e267b1d730522270db520a9573
Nadav Naveh <nadav.naveh.23@gmail.com>
2025-06-22 19:30:13 +0300

    Implemented UpperCaseBlock class

---
+++ Generics/Generics/UpperCaseBlock.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class UpperCaseBlock<T> : IBlock<string> 
+    {
+        public string Process(string input)
+        {
+            return input.ToUpper();
+        }
+    }
+}
