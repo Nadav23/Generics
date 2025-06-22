2f4c1b10b4110ac2e5cdcde56fc38a07fd9be12a
Nadav Naveh <nadav.naveh.23@gmail.com>
2025-06-22 19:13:09 +0300

    Implemented IDataFlow Interface

---
+++ Generics/Generics/Block.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class Block<T> : IBlock<T> where T : class
+    {
+        public T Process(T input)
+        {
+            return input;
+        }
+    }
+}
---
+++ Generics/Generics/DataFlow.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class DataFlow<T> : IDataFlow<T> where T : class
+    {
+        public List<IBlock<T>> flow;
+        public T RunFlow(T input)
+        {
+            T output = input;
+
+            foreach (IBlock<T> block in flow)
+            {
+                output = block.Process(input);
+
+            }
+
+            return output;
+
+        }
+
+        public void AddBlock(IBlock<T> block)
+        {
+            flow.Add(block);
+
+        }
+    }
+
+}
